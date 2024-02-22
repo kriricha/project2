328476555ecea0183ccf889e2016afd411a5db66
kriricha <kriricha0@gmail.com>
2024-02-22 18:54:53 +0530

    Revert "my first one"(ignore this commit)
    This reverts commit d272f296b17d0c7a4f851fba4830be93bbb5f73c.

--- src/main/java/com/example/SpringBootWithElastic/repo/ElasticRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringBootWithElastic.repo;
-
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.example.SpringBootWithElastic.model.Elastic;
-public interface ElasticRepository extends ElasticsearchRepository<Elastic, String> {
-}
